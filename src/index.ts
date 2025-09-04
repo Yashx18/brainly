59ee582e77c3c30717caf9b91ab82d66f74e727e
ken <yash20004x17@gmail.com>
2025-09-04 04:36:30 +0000

    ENV update

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,6 @@ async function connectdb() {
 
 connectdb();
 app.listen(PORT, () => {
-  console.log(`Server is running at 'http://localhost:${PORT}'`);
+  // console.log(`Server is running at 'http://localhost:${PORT}'`);
 });
 // export default app;
